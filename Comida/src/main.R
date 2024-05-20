69a314080adac5967dbf9acea1abe4c5208a4e1b
jonecto <jonectado@gmail.com>
2024-05-19 20:53:17 -0500

    Comida!!!!!!!!!

--- Comida/src/main.R
+++ Comida/src/main.R
@@ -1,20 +1,18 @@
 #install.packages("summarytools")
 library(summarytools)
 library(ggplot2)
+library(lmtest)
+
 
 
 getwd()
-setwd("C:/Users/sofia/Documents/Hackaton Datos/Comida/src/")
+setwd("C:/Users/jonec/Documents/Dev/AEDenso/Comida/src")
 
 
 datos_comida <- read.csv("../input/epi_r.csv")
 
-colnames(datos_comida)
-
-unique(datos_comida)
-
-
-# Filtrar las filas donde 'calories' es mayor que 500
+# Filtrar las filas donde el rating es de 5.0 para tener
+# mejor conocimiento de los datos objetivo
 datos_filtrados <- datos_comida[datos_comida$rating == 5.0, ]
 
 
@@ -56,7 +54,7 @@ if (length(columnas_altas_media) > 0) {
 # Segundo acercamiento ----------------------------------------------------
 
 # Crear una versión limpia de datos_filtrados sin las columnas innecesarias
-datos_limpios <- subset(datos_filtrados, select = 1:6)
+datos_limpios <- subset(datos_comida, select = 1:6)
 
 # Mostrar las primeras filas de datos_limpios para verificar
 head(datos_limpios)
@@ -69,26 +67,65 @@ ggplot(datos_limpios, aes(x = calories)) +
   geom_histogram(binwidth = 50, fill = "skyblue", color = "black") +
   labs(title = "Distribución de Calorías")
 
-# Crear un histograma para la proteína
-ggplot(datos_limpios, aes(x = protein)) +
-  geom_histogram(binwidth = 5, fill = "lightgreen", color = "black") +
-  labs(title = "Distribución de Proteína")
-
-# Crear un histograma para la grasa
-ggplot(datos_limpios, aes(x = fat)) +
-  geom_histogram(binwidth = 5, fill = "salmon", color = "black") +
-  labs(title = "Distribución de Grasa")
-
-# Crear un histograma para el sodio
-ggplot(datos_limpios, aes(x = sodium)) +
-  geom_histogram(binwidth = 50, fill = "orange", color = "black") +
-  labs(title = "Distribución de Sodio")
-
 #Hay datos muy atipicos en la tabla, por lo cual se tomarán solo el cuantil 95.
-
-quantile(datos_limpios$calories, 0.95)
 
 # Filtrar los datos para incluir solo aquellos dentro del cuantil 95
 datos_limpios_cuantil_95 <- datos_limpios[apply(datos_limpios[, 3:ncol(datos_limpios)], 1, function(row) {
   all(row <= 1407)
 }), ]
+
+
+# Analisis descriptivo ----------------------------------------------------
+
+# Mostrar un resumen descriptivo de las variables
+summary(datos_limpios_cuantil_95)
+
+# Crear un histograma para las calorías
+ggplot(datos_limpios_cuantil_95, aes(x = calories)) +
+  geom_histogram(binwidth = 50, fill = "skyblue", color = "black") +
+  labs(title = "Distribución de Calorías")
+
+# Crear un histograma para la proteína
+ggplot(datos_limpios_cuantil_95, aes(x = protein)) +
+  geom_histogram(binwidth = 5, fill = "lightgreen", color = "black") +
+  labs(title = "Distribución de Proteína")
+
+# Crear un histograma para la grasa
+ggplot(datos_limpios_cuantil_95, aes(x = fat)) +
+  geom_histogram(binwidth = 5, fill = "salmon", color = "black") +
+  labs(title = "Distribución de Grasa")
+
+
+# Modelos -----------------------------------------------------------------
+
+# Ajustar el modelo de regresión lineal Todas las variables
+modelo <- lm(rating ~ fat, data = datos_limpios_cuantil_95)
+
+# Resumen del modelo
+summary(modelo)
+
+# Prueba de normalidad de los residuos
+shapiro.test(residuals(modelo))
+
+# Prueba de homocedasticidad
+bptest(modelo)
+
+# Gráficos de diagnóstico del modelo
+par(mfrow = c(2, 2))
+plot(modelo)
+
+# Gráfico de boxplot para sodium vs rating
+ggplot(datos_limpios_cuantil_95, aes(x = as.factor(rating), y = sodium)) +
+  geom_boxplot(fill = "blue", alpha = 0.5) +
+  labs(title = "Sodium vs Rating",
+       x = "Rating",
+       y = "Sodium") +
+  theme_minimal()
+
+ggplot(datos_limpios_cuantil_95, aes(x = as.factor(rating), y = fat)) +
+  geom_boxplot(fill = "blue", alpha = 0.5) +
+  labs(title = "Fat vs Rating",
+       x = "Rating",
+       y = "Fat") +
+  theme_minimal()
+
