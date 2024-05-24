364437a866cbc2eac06d09eaa9d37b870748e3cc
davinchooo <112956569+davinchooo@users.noreply.github.com>
2024-05-23 19:05:24 -0500

    Add files via upload

---
+++ cancerdata/src/analysis/regresion.R
@@ -0,0 +1,35 @@
+model1 <- lm(data = reg_houses,
+            SalePrice ~ log(LotArea) + year_since_build +year_since_remod + CentralAir + OverallQual)
+
+summary(model1)
+
+# Buena especcificacion del modelo:
+  # R^2
+  # R^2 ajustado
+  # resset.ramset -> Buscar una mejor tranf.
+  
+resettest(model1)
+
+# Estudio de los errores
+
+mean(model1$residuals)
+
+plot(model1$residuals, pch  = 20)
+
+hist(model1$residuals, breaks = 40)
+
+# Heterocedasticidad
+# H0: var cte ~ x^2(p) Homocedasticidad
+# Ha:                  Heterocedasticidad 
+#
+# Si el p valor es muy pequeño, rechazamos
+
+bptest(model1)
+
+# Normalidad de los errores
+
+shapiro.test(model1$residuals)
+
+# Estudio de los residuos
+plot(model1 )
+# Q-Q plot: En y tiene los residuios estandarizados, y en x los quantiles 
