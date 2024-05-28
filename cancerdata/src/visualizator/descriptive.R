ed1f530cba41ac6ef7b11e2ac4dc2a636224c203
davinchooo <112956569+davinchooo@users.noreply.github.com>
2024-05-27 23:01:29 -0500

    Update descriptive.R

--- cancerdata/src/visualizator/descriptive.R
+++ cancerdata/src/visualizator/descriptive.R
@@ -2,21 +2,33 @@ faces(base_cancer, fill = FALSE)
 
 Total_Females <- ggplot(base_cancer, aes(y = Total, x = Females)) +
   geom_point() +
+  labs(title = "Total_Females",
+       x = "Females",
+       y = "Total") +
   xlim(c(0, 5000)) +
   ylim(c(0, 5000))
 
 Total_Males <- ggplot(base_cancer, aes(y = Total, x = Males)) +
   geom_point() +
+  labs(title = "Total_Males",
+       x = "Males",
+       y = "Total") +
   xlim(c(0, 2500)) +
   ylim(c(0, 5000))
 
 Total_Whites <- ggplot(base_cancer, aes(y = Total, x = Whites)) +
   geom_point() +
+  labs(title = "Total_Whites",
+       x = "Whites",
+       y = "Total") +
   xlim(c(0, 5000)) +
   ylim(c(0, 5000))
 
 Total_Blacks <- ggplot(base_cancer, aes(y = Total, x = Blacks)) +
   geom_point() +
+  labs(title = "Total_Blacks",
+       x = "Blacks",
+       y = "Total") +
   xlim(c(0, 5000)) +
   ylim(c(0, 5000))
 
@@ -24,7 +36,7 @@ Total_Blacks <- ggplot(base_cancer, aes(y = Total, x = Blacks)) +
 
 Male_Female <- ggplot(base_cancer, aes(x = Males, y = Females)) +
   geom_point() +
-  labs(title = "Gráfico de Dispersión",
+  labs(title = "Male_Female",
        x = "Males",
        y = "Females") +
   theme_minimal() + 
@@ -33,7 +45,7 @@ Male_Female <- ggplot(base_cancer, aes(x = Males, y = Females)) +
 
 Black_White <- ggplot(base_cancer, aes(x = Whites, y = Blacks)) +
   geom_point() +
-  labs(title = "Gráfico de Dispersión",
+  labs(title = "Black_White",
        x = "Whites",
        y = "Blacks") +
   theme_minimal() + 
