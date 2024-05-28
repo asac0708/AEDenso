4a5dc1adbac71b155d248dbd3468e420f530d85c
davinchooo <112956569+davinchooo@users.noreply.github.com>
2024-05-27 21:24:16 -0500

    Update etl.R

--- cancerdata/src/preprocessor/etl.R
+++ cancerdata/src/preprocessor/etl.R
@@ -1,7 +1,8 @@
 base_cancer <- data_cancer[, 2:8] # Quitando la columna de las jurisdicciones
 rownames(base_cancer) <- data_cancer$Jurisdiction # Cambiarle el nombre a las filas
 base_cancer <- base_cancer[-1, ] #Quitandole variables que alteran los resultados
-
+data_cancer <- data_cancer[-1, ] #Quitandole variables que alteran los resultados
+rownames(base_cancer) <- data_cancer$Jurisdiction # Cambiarle el nombre a las filas
 
 pca <- PCA(base_cancer) # Cuantas dimensiones queremos obtener 
 
@@ -31,30 +32,15 @@ pca$ind$contrib
 distancias <- dist(pca$var$coord)
 arbol <- hclust(distancias)
 plot(arbol)
-# grupos <- cutree(arbol,3)
-# 
-# 
-# indices <- pca$var$coord %>%data.frame()
-# indices[, 'grupo'] <- factor(grupos)
-# 
-# ggplot(data = indices, aes(x = Dim.1, y = Dim.2, color = grupo))+
-#   geom_point()
-# 
-# 
-# grupos2 <- kmeans(indices, 3)
-# indices[, 'grupos2'] <- factor(grupos2$cluster)
-# 
-# ggplot(data = indices, aes(x = Dim.1, y = Dim.2, color = grupos2))+
-#   geom_point()
+
 
 
 # Tabla de contingencia ---------------------------------------------------
 
 Total_Males <- table(base_cancer$Total, base_cancer$Males)
-Total_Females <- table(base_cancer$Total, base_cancer$Males)
-Total_Blacks <- table(base_cancer$Total, base_cancer$Males)
-Total_Whites <- table(base_cancer$Total, base_cancer$Males)
-Total_Males <- table(base_cancer$Total, base_cancer$Males)
+Total_Females <- table(base_cancer$Total, base_cancer$Females)
+Total_Blacks <- table(base_cancer$Total, base_cancer$Blacks)
+Total_Whites <- table(base_cancer$Total, base_cancer$Whites)
 
 
 
@@ -62,8 +48,6 @@ chisq.test(Total_Males)
 chisq.test(Total_Females)
 chisq.test(Total_Blacks)
 chisq.test(Total_Whites)
-
-fisher.test(Total_Blacks)
 
 
 
@@ -98,12 +82,9 @@ plot(arbol)
 # Tabla de contingencia
 
 Total_Males <- table(base_cancer2$Total, base_cancer2$Males)
-Total_Females <- table(base_cancer2$Total, base_cancer2$Males)
-Total_Blacks <- table(base_cancer2$Total, base_cancer2$Males)
-Total_Whites <- table(base_cancer2$Total, base_cancer2$Males)
-Total_Males <- table(base_cancer2$Total, base_cancer2$Males)
-
-
+Total_Females <- table(base_cancer2$Total, base_cancer2$Females)
+Total_Blacks <- table(base_cancer2$Total, base_cancer2$Blacks)
+Total_Whites <- table(base_cancer2$Total, base_cancer2$Whites)
 
 chisq.test(Total_Males)
 chisq.test(Total_Females)
